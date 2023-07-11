404c2673220a49aec47cd3a04220e16e74b90ca9
cinchtail <cinchtail@gmail.com>
2023-07-11 18:45:08 -0400

    pitcher plant crop and carrot crop loot table fixes

--- src/main/java/net/cinchtail/cinchcraft/util/ModBlockTags.java
+++ src/main/java/net/cinchtail/cinchcraft/util/ModBlockTags.java
@@ -1,9 +1,7 @@
 package net.cinchtail.cinchcraft.util;
-import net.cinchtail.cinchcraft.Cinchcraft;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
-import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
 
 public class ModBlockTags {
