952a71ec2b480753588ef140ea97a550ff7c7917
cinchtail <cinchtail@gmail.com>
2023-07-29 14:25:14 -0400

    working on azalea and bee keeper villager

--- src/main/java/net/cinchtail/cinchcraft/util/ModWoodTypes.java
+++ src/main/java/net/cinchtail/cinchcraft/util/ModWoodTypes.java
@@ -1,9 +1,8 @@
 package net.cinchtail.cinchcraft.util;
 
 import net.cinchtail.cinchcraft.Cinchcraft;
-import net.minecraft.world.level.block.state.properties.BlockSetType;
 import net.minecraft.world.level.block.state.properties.WoodType;
 
 public class ModWoodTypes {
-    public static final WoodType AZALEA = WoodType.register(new WoodType(Cinchcraft.MOD_ID + ":azalea", BlockSetType.OAK));
+    public static final WoodType AZALEA = WoodType.register(new WoodType(Cinchcraft.MOD_ID + ":azalea", ModBlockSetType.AZALEA));
 }
