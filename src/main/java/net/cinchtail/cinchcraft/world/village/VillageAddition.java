3468496a8c852bfaaa36279585e18bb543808395
cinchtail <cinchtail@gmail.com>
2023-08-03 22:34:22 -0400

    finally got all beekeeper houses spawning in villages

--- src/main/java/net/cinchtail/cinchcraft/world/village/VillageAddition.java
+++ src/main/java/net/cinchtail/cinchcraft/world/village/VillageAddition.java
@@ -51,14 +51,14 @@ public class VillageAddition {
 
         addBuildingToPool(templatePoolRegistry, processorListRegistry,
                 new ResourceLocation("minecraft:village/plains/houses"),
-                "cinchcraft:village_addition/plains_bee_keeper_house", 12);
+                "cinchcraft:village_addition/plains_bee_keeper_house", 10);
 
         addBuildingToPool(templatePoolRegistry, processorListRegistry,
                 new ResourceLocation("minecraft:village/taiga/houses"),
-                "cinchcraft:village_addition/taiga_bee_keeper_house", 12);
+                "cinchcraft:village_addition/taiga_bee_keeper_house", 15);
 
         addBuildingToPool(templatePoolRegistry, processorListRegistry,
                 new ResourceLocation("minecraft:village/savanna/houses"),
-                "cinchcraft:village_addition/savanna_bee_keeper_house", 9);
+                "cinchcraft:village_addition/savanna_bee_keeper_house", 7);
     }
 }
