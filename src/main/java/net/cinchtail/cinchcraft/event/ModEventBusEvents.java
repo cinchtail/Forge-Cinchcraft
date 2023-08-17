6ab1f08ea562ae67375a22e7ddc518acaa852459
cinchtail <cinchtail@gmail.com>
2023-08-17 12:19:13 -0400

    fixed critical issue with azalea boats

--- src/main/java/net/cinchtail/cinchcraft/event/ModEventBusEvents.java
+++ src/main/java/net/cinchtail/cinchcraft/event/ModEventBusEvents.java
@@ -4,15 +4,18 @@ import net.cinchtail.cinchcraft.Cinchcraft;
 import net.cinchtail.cinchcraft.entity.layers.ModModelLayers;
 import net.minecraft.client.model.BoatModel;
 import net.minecraft.client.model.ChestBoatModel;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = Cinchcraft.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModEventBusEvents {
+
+    @OnlyIn(Dist.CLIENT)
     @SubscribeEvent
     public static void registerLayers(EntityRenderersEvent.RegisterLayerDefinitions event) {
-
         event.registerLayerDefinition(ModModelLayers.AZALEA_BOAT_LAYER, BoatModel::createBodyModel);
         event.registerLayerDefinition(ModModelLayers.AZALEA_CHEST_BOAT_LAYER, ChestBoatModel::createBodyModel);
 
